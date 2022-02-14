ac8074541adf6c98b2b8e3a0f443b5a830b25200
Ajonye Paul <ajonyechase@gmail.com>
2022-02-14 12:52:04 +0100

    just configured tailwind css to purge unused css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     purge: [
         "./storage/framework/views/*.php",
-        "./resources/**/**/*.blade.php",
+        "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
     ],
